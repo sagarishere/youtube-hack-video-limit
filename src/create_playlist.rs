6023d40eb4d80b760c7e432e492bd6c594468124
sagarishere <5121817+sagarishere@users.noreply.github.com>
2024-05-30 09:29:08 +0300

    Create create_playlist.rs
    the code to create playlist

---
+++ src/create_playlist.rs
@@ -0,0 +1,82 @@
+use mp4::Mp4Reader;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::path::Path;
+use std::time::Duration;
+
+fn format_duration(dur: Duration) -> String {
+    let total_seconds = dur.as_secs();
+    let hours = total_seconds / 3600;
+    let minutes = (total_seconds % 3600) / 60;
+    let seconds = total_seconds % 60;
+    format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+}
+
+fn get_mp4_duration(file_path: &Path) -> Result<Duration, mp4::Error> {
+    let file = File::open(file_path)?;
+    let size = file.metadata()?.len();
+    let reader = BufReader::new(file);
+    let mp4 = Mp4Reader::read_header(reader, size)?;
+    Ok(mp4.duration())
+}
+
+fn get_mp4_files_sorted(folders: Vec<String>) -> std::io::Result<()> {
+    let mut playlist = BufWriter::new(File::create("playlist.txt")?);
+    writeln!(playlist, "⭐️ Contents ⭐️")?;
+
+    let mut course_time_passed = Duration::new(0, 0);
+
+    for folder in folders {
+        writeln!(playlist, "\n📂 {}", folder)?;
+
+        let folder_path = Path::new(&folder);
+        let mut files: Vec<_> = fs::read_dir(folder_path)?
+            .filter_map(|entry| {
+                let entry = entry.ok()?;
+                let path = entry.path();
+                if path.is_file() && path.extension()? == "mp4" {
+                    Some(entry.path())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        files.sort();
+
+        let mut course_time = course_time_passed;
+
+        for file in files {
+            match get_mp4_duration(&file) {
+                Ok(duration_secs) => {
+                    writeln!(
+                        playlist,
+                        "⌨️ ({}) {:?}",
+                        format_duration(course_time),
+                        file.file_name().unwrap()
+                    )?;
+                    course_time += duration_secs;
+                }
+                Err(e) => {
+                    eprintln!("Error reading duration for file {:?}: {}", file, e);
+                }
+            }
+        }
+
+        course_time_passed = course_time;
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("folders.txt")?;
+    let reader = BufReader::new(file);
+    let folders: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+
+    if let Err(e) = get_mp4_files_sorted(folders) {
+        eprintln!("Error: {}", e);
+    }
+
+    Ok(())
+}
