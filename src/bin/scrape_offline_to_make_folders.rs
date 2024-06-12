8392e7978c1a5e85d0c80c2c80af84bc979f9d4b
sagarishere <5121817+sagarishere@users.noreply.github.com>
2024-06-12 15:24:07 +0300

    Folder Parsing: Selector Updated

--- src/bin/scrape_offline_to_make_folders.rs
+++ src/bin/scrape_offline_to_make_folders.rs
@@ -4,32 +4,26 @@ use std::io::Read;
 use std::path::Path;
 
 fn main() {
-    // Path to your local HTML file
     let path = Path::new("./doc.html");
 
-    // Check if the file exists
     if !path.exists() {
         eprintln!("File does not exist at the specified path.");
         return;
     }
 
-    // Read the HTML file
     let mut file = File::open(&path).expect("Could not open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("Could not read file");
 
-    // Parse the HTML document
     let document = Html::parse_document(&contents);
 
-    // Create a selector for h2 elements
-    let h2_selector = Selector::parse("h2").expect("Could not create selector");
+    let h2_selector = Selector::parse("h2[class*=\"heading\"]").expect("Could not create selector");
 
     // Find all h2 elements that contain '(' and ')'
     let h2_elements: Vec<_> = document
         .select(&h2_selector)
-        .filter_map(|el| el.text().next().map(|text| text.trim().to_string()))
-        .filter(|text| text.contains('(') && text.contains(')'))
+        .filter_map(|el| el.text().next().map(|text| text.trim().to_string().split("(").collect::<Vec<_>>()[0].to_string()))
         .map(|text| {
             // Remove everything after the space and '('
             if let Some(index) = text.find(" (") {
